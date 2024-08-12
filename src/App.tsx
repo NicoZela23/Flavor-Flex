3912638b31bb04442ebb8c4cd02d9800f1ad78fa
NicoZela <fezelayani@gmail.com>
2024-08-12 11:54:46 -0400

    Testing

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        This is just a test to manage the develop environment with Vite and React. You can read the docs
       </p>
     </>
   )
