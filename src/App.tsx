aeb36d1d1271abd6a5481c05668f1aa2e435f230
Walter Vaquera <walter.bvr@gmail.com>
2024-08-28 10:53:40 -0400

    feat(favorites): added buttons to add/remove a recipe from the user's list of favorites

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,20 @@ import Navbar from './components/organisms/Navbar'
 import Footer from './components/organisms/Footer';
 import Home from './components/templates/Home';
 import RecipeCardType from './types/RecipeCardType';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import RecipePage from './components/organisms/RecipePage';
 import Favourites from './components/organisms/Favorites';
 
 function App() {
   const [searchQuery, setSearchQuery] = useState<string>('');
   const inputField = useRef<HTMLInputElement>(null);
-  const [savedRecipes, setSavedRecipes] = useState(() => {
+  const navigate = useNavigate();
+  
+  const [savedRecipes, setSavedRecipes] = useState<RecipeCardType[]>(() => {
     const localData = localStorage.getItem("recipes");
     return localData ? JSON.parse(localData) : [];
-  });
+  });  
   
-  // New state for recipes, loading, and error
   const [recipes, setRecipes] = useState<RecipeCardType[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
@@ -32,6 +33,7 @@ function App() {
     setSearchQuery("");
     setRecipes([]);
     setError("");
+    navigate("/");
   };
 
   const getData = async (searchQuery: string) => {
@@ -56,6 +58,29 @@ function App() {
     }
   };
 
+  const favouriteHandler = (id: string) => {
+    fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const recipe: RecipeCardType = data.data.recipe;
+        checkLocalData(recipe);
+      });
+
+    navigate("/favourites");
+  };
+
+  const checkLocalData = (data: RecipeCardType) => {
+    const localData = JSON.parse(localStorage.getItem("recipes") || '[]') as RecipeCardType[];
+    const existedData = localData.some((item) => item.id === data.id);
+  
+    if (!existedData) {
+      setSavedRecipes([...savedRecipes, data]);
+    } else {
+      const filteredData = localData.filter((item) => item.id !== data.id);
+      setSavedRecipes(filteredData);
+    }
+  };  
+
   return (
     <div className="app min-h-screen bg-rose-50 text-gray-600 text-lg">
       <Navbar
@@ -77,7 +102,12 @@ function App() {
           />
           <Route
             path="/recipe-item/:id"
-            element={<RecipePage/>}
+            element={
+              <RecipePage
+                favouriteHandler={favouriteHandler}
+                savedRecipes={savedRecipes}
+              />
+            }
           />
         </Routes>
       </main>
